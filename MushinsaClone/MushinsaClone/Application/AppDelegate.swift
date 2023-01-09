4378d8346773849f9693b31fc153d37416fc9287
Damagucci-Juice <jsr9874@gmail.com>
2023-01-09 12:20:02 +0900

    init: 무신사 클론 프로젝트

---
+++ MushinsaClone/MushinsaClone/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MushinsaClone
+//
+//  Created by YEONGJIN JANG on 2023/01/09.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ MushinsaClone/MushinsaClone/Presentation/MushinsaScene/ItemList/View/ItemListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ItemListViewController.swift
+//  MushinsaClone
+//
+//  Created by YEONGJIN JANG on 2023/01/09.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
